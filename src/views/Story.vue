1954b9e52102298f7a055d93ab9459f04a1eba0d
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2021-03-09 23:37:28 +0900

    Show comment

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -1,18 +1,22 @@
 <template>
-  <div>
+  <div v-if="story">
     <h3>{{ story.title }}</h3>
     <span>by {{ story.by }}</span>
-    <a :href="story.url" st,  yle="margin-left: 0.5rem">link</a>
+    <a :href="story.url" style="margin-left: 0.5rem">link</a>
+  </div>
+  <div v-if="comment">
+    {{ JSON.stringify(comment) }}
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import { fetchStory, Story } from "../api";
+import { Comment, fetchComment as _fetchComment, fetchStory, Story } from "../api";
 
 export default defineComponent({
   setup() {
     const story = ref<Story | null>(null)
+    const comment = ref<Comment | null>(null);
 
     const init = async () => {
       const res = await fetchStory(1);
@@ -20,7 +24,13 @@ export default defineComponent({
     }
     init();
 
-    return { story };
+    const fetchComment = async () => {
+      const res = await _fetchComment(1);
+      comment.value = res;
+    }
+    fetchComment();
+
+    return { story, comment };
   }
 });
 </script>
