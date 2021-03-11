523d3a33324d64b400bbc38bc200b72fc9ed4c4e
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2021-03-11 23:54:46 +0900

    Update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,21 @@
   <div>
     Home
     <p><router-link to="/story">story</router-link></p>
+    <ul>
+      <li v-for="story in stories" :key="story.id"></li>
+    </ul>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "@vue/runtime-core";
+
+export default defineComponent({
+  setup() {
+    const stories = ref([])
+    return {
+      stories
+    };
+  }
+});
+</script>
