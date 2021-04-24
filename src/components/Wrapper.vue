a76dcfd20f07d32e310f6331627a5917c2ef1e4a
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2021-04-24 23:33:01 +0900

    Add wrapper

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  props: {
+    condition: {
+      type: Function,
+      required: true
+    }
+  },
+  setup(props, { slots }) {
+    return () => (props.condition() ? slots.default?.() : null);
+  }
+})
+</script>
